6f4672e8a52519993427c28c222f6560246cf53e
Zhiting Zhu <zhitingz@cs.utexas.edu>
2023-02-11 23:49:53 -0600

    script to install mamba
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

---
+++ mamba/download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh
+bash Mambaforge-Linux-x86_64.sh -b
+rm Mambaforge-Linux-x86_64.sh
