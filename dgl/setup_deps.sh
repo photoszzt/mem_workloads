ad5632787fdc3dbc6bbbcb2edbb2c5cafdbd842f
zhiting zhu <zhitingz@cs.utexas.edu>
2023-02-15 15:06:55 -0600

    source to get mamba
    Signed-off-by: zhiting zhu <zhitingz@cs.utexas.edu>

--- dgl/setup_deps.sh
+++ dgl/setup_deps.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+source $HOME/mambaforge/bin/activate
 mamba create -n dgl python=3.9
 mamba install -y -n dgl pytorch torchvision torchaudio cpuonly -c pytorch
 mamba install -y -n dgl -c dglteam dgl
