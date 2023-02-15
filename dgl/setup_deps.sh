36c3ec33667dddfd2d1ba60afd2cdacba900b081
zhiting zhu <zhitingz@cs.utexas.edu>
2023-02-15 15:35:13 -0600

    install ogb
    Signed-off-by: zhiting zhu <zhitingz@cs.utexas.edu>

--- dgl/setup_deps.sh
+++ dgl/setup_deps.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source $HOME/mambaforge/bin/activate
-mamba create -n dgl python=3.9
+mamba create -y -n dgl python=3.9
 mamba install -y -n dgl pytorch torchvision torchaudio cpuonly -c pytorch
 mamba install -y -n dgl -c dglteam dgl
+mamba install -y -n dgl ogb
