b3e93172b1fc7df6a3a465d14facb4416315d9e2
zhiting zhu <zhitingz@cs.utexas.edu>
2023-02-15 14:47:29 -0600

    fix activate
    Signed-off-by: zhiting zhu <zhitingz@cs.utexas.edu>

--- mamba/download.sh
+++ mamba/download.sh
@@ -4,4 +4,4 @@
 wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh
 bash Mambaforge-Linux-x86_64.sh -b
 rm Mambaforge-Linux-x86_64.sh
-source $HOME/mambaforge/activate && mamba init
+source $HOME/mambaforge/bin/activate && mamba init
