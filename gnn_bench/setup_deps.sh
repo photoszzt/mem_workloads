cdcd5bf2bc9174c696ab638b12aa51ca9ac3ae59
Zhiting Zhu <zhitingz@cs.utexas.edu>
2023-02-18 21:12:31 -0600

    backup local modification
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- gnn_bench/setup_deps.sh
+++ gnn_bench/setup_deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mamba create -n benchmark_gnn python=3.7.4
-mamba activate benchmark_gnn
-mamba install pandas
-mamba env update -f ./benchmarking-gnns
+mamba create -n benchmark_gnn python=3.7
+mamba install -n benchmark_gnn pandas
+[ -d "./benchmarking-gnns" ] || git clone https://github.com/graphdeeplearning/benchmarking-gnns.git
+mamba env update --name=benchmark_gnn --file=./environment_cpu.yml
