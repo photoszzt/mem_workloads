8860aa0389bf77daa265835ee3deeae703545259
zhiting zhu <zhitingz@cs.utexas.edu>
2023-03-01 21:44:59 -0600

    install pynuma
    Signed-off-by: zhiting zhu <zhitingz@cs.utexas.edu>

--- dgl/setup_deps.sh
+++ dgl/setup_deps.sh
@@ -5,3 +5,4 @@ mamba create -y -n dgl python=3.9
 mamba install -y -n dgl pytorch torchvision torchaudio cpuonly -c pytorch
 mamba install -y -n dgl -c dglteam dgl
 mamba install -y -n dgl ogb
+$HOME/mambaforge/envs/dgl/bin/python3 -m pip install --upgrade py-libnuma
