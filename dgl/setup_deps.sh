66d91830bbbeece30542e3b3f54d0a45a07e9118
zhitingz <zhitingz@node-0.vhive-test.ut-osa-pg0.wisc.cloudlab.us>
2023-03-02 13:37:07 -0600

    install jemalloc
    Signed-off-by: zhitingz <zhitingz@node-0.vhive-test.ut-osa-pg0.wisc.cloudlab.us>

--- dgl/setup_deps.sh
+++ dgl/setup_deps.sh
@@ -5,4 +5,5 @@ mamba create -y -n dgl python=3.9
 mamba install -y -n dgl pytorch torchvision torchaudio cpuonly -c pytorch
 mamba install -y -n dgl -c dglteam dgl
 mamba install -y -n dgl ogb
+mamba install -y -n dgl -c conda-forge jemalloc
 $HOME/mambaforge/envs/dgl/bin/python3 -m pip install --upgrade py-libnuma
