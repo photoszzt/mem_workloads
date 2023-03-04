0b2fdb34fc0c2a7f852ffdeebae38926c5eca99b
zhiting zhu <zhitingz@cs.utexas.edu>
2023-03-03 22:10:53 -0600

    download and compile jemalloc
    Signed-off-by: zhiting zhu <zhitingz@cs.utexas.edu>

--- dgl/setup_deps.sh
+++ dgl/setup_deps.sh
@@ -6,3 +6,9 @@ mamba install -y -n dgl pytorch torchvision torchaudio cpuonly -c pytorch
 mamba install -y -n dgl -c dglteam dgl
 mamba install -y -n dgl ogb
 $HOME/mambaforge/envs/dgl/bin/python3 -m pip install --upgrade py-libnuma
+
+{ [ -d jemalloc ] && cd jemalloc && git checkout tags/5.3.0; } || { git clone https://github.com/jemalloc/jemalloc.git && cd jemalloc && git checkout tags/5.3.0; }
+cd jemalloc
+./configure --enable-prof --enable-stats
+make -j$(nproc)
+sudo make install
