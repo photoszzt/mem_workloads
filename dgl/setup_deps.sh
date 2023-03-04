d36a6ed180266043b48230e574c7f3fcde4edfff
zhitingz <zhitingz@node-0.vhive-test.ut-osa-pg0.wisc.cloudlab.us>
2023-03-03 22:26:54 -0600

    fix jemalloc build
    Signed-off-by: zhitingz <zhitingz@node-0.vhive-test.ut-osa-pg0.wisc.cloudlab.us>

--- dgl/setup_deps.sh
+++ dgl/setup_deps.sh
@@ -8,8 +8,8 @@ mamba install -y -n dgl ogb
 mamba install -y -n dgl -c conda-forge jemalloc
 $HOME/mambaforge/envs/dgl/bin/python3 -m pip install --upgrade py-libnuma
 
-{ [ -d jemalloc ] && cd jemalloc && git checkout tags/5.3.0; } || { git clone https://github.com/jemalloc/jemalloc.git && cd jemalloc && git checkout tags/5.3.0; }
-cd jemalloc
+[ -d $HOME/jemalloc-5.3.0 ] || { wget https://github.com/jemalloc/jemalloc/releases/download/5.3.0/jemalloc-5.3.0.tar.bz2 -O $HOME/jemalloc-5.3.0.tar.bz2 && cd $HOME && tar -xvf $HOME/jemalloc-5.3.0.tar.bz2 ; }
+cd $HOME/jemalloc-5.3.0
 ./configure --enable-prof --enable-stats
 make -j$(nproc)
 sudo make install
