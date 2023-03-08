2fc0c6773dc12c41d002d5af35816c48103a8ff9
zhitingz <zhitingz@node-0.vhive-test.ut-osa-pg0.wisc.cloudlab.us>
2023-03-08 12:42:08 -0600

    use conda to install deps; seems mamba forge had some problem
    Signed-off-by: zhitingz <zhitingz@node-0.vhive-test.ut-osa-pg0.wisc.cloudlab.us>

---
+++ conda/download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b
+rm Miniconda3-latest-Linux-x86_64.sh
+source $HOME/miniconda3/bin/activate && conda init
--- dgl/setup_deps.sh
+++ dgl/setup_deps.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-source $HOME/mambaforge/bin/activate
-mamba create -y -n dgl python=3.9
-mamba install -y -n dgl pytorch torchvision torchaudio cpuonly -c pytorch
-mamba install -y -n dgl -c dglteam dgl
-mamba install -y -n dgl ogb
-mamba install -y -n dgl -c conda-forge jemalloc
-$HOME/mambaforge/envs/dgl/bin/python3 -m pip install --upgrade py-libnuma
+source $HOME/miniconda3/bin/activate
+conda create -y -n dgl python=3.9
+conda install -y -n dgl pytorch torchvision torchaudio cpuonly -c pytorch
+conda install -y -n dgl -c dglteam dgl
+conda install -y -n dgl -c conda-forge ogb
+$HOME/miniconda3/envs/dgl/bin/python3 -m pip install --upgrade py-libnuma
 
 [ -d $HOME/jemalloc-5.3.0 ] || { wget https://github.com/jemalloc/jemalloc/releases/download/5.3.0/jemalloc-5.3.0.tar.bz2 -O $HOME/jemalloc-5.3.0.tar.bz2 && cd $HOME && tar -xvf $HOME/jemalloc-5.3.0.tar.bz2 ; }
 cd $HOME/jemalloc-5.3.0
