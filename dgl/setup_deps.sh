175b5bead9d2c691ba36c8287bee2302648d71ea
zhitingz <zhitingz@cs.utexas.edu>
2023-04-19 04:07:35 +0000

    don't need to build jemalloc
    Signed-off-by: zhitingz <zhitingz@cs.utexas.edu>

--- dgl/setup_deps.sh
+++ dgl/setup_deps.sh
@@ -7,8 +7,8 @@ conda install -y -n dgl -c dglteam dgl
 conda install -y -n dgl -c conda-forge ogb
 $HOME/miniconda3/envs/dgl/bin/python3 -m pip install --upgrade py-libnuma
 
-[ -d $HOME/jemalloc-5.3.0 ] || { wget https://github.com/jemalloc/jemalloc/releases/download/5.3.0/jemalloc-5.3.0.tar.bz2 -O $HOME/jemalloc-5.3.0.tar.bz2 && cd $HOME && tar -xvf $HOME/jemalloc-5.3.0.tar.bz2 ; }
-cd $HOME/jemalloc-5.3.0
-./configure --enable-prof --enable-stats
-make -j$(nproc)
-sudo make install
+# [ -d $HOME/jemalloc-5.3.0 ] || { wget https://github.com/jemalloc/jemalloc/releases/download/5.3.0/jemalloc-5.3.0.tar.bz2 -O $HOME/jemalloc-5.3.0.tar.bz2 && cd $HOME && tar -xvf $HOME/jemalloc-5.3.0.tar.bz2 ; }
+# cd $HOME/jemalloc-5.3.0
+# ./configure --enable-prof --enable-stats
+# make -j$(nproc)
+# sudo make install
