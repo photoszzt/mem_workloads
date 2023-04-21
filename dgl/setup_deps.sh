d76ab75dd397578f59337e7f4c07b5d662d10398
zhiting zhu <zhitingz@cs.utexas.edu>
2023-04-21 18:04:58 -0500

    update deps
    Signed-off-by: zhiting zhu <zhitingz@cs.utexas.edu>

--- dgl/setup_deps.sh
+++ dgl/setup_deps.sh
@@ -2,10 +2,14 @@
 
 source $HOME/miniconda3/bin/activate
 conda create -y -n dgl python=3.9
+conda install -y -n dgl -c conda-forge mkl libuv \
+    'numpy>=1.16' 'pandas>=0.24' 'urllib3>=1.24.0' 'scikit-learn>=0.20' 
+conda_env_path_arr=($(conda env list | grep pt_custom_gloo))
+conda_env_path=${conda_env_path_arr[${#conda_env_path_arr[@]}-1]}
 conda install -y -n dgl pytorch torchvision torchaudio cpuonly -c pytorch
 conda install -y -n dgl -c dglteam dgl
-conda install -y -n dgl -c conda-forge ogb
-$HOME/miniconda3/envs/dgl/bin/python3 -m pip install --upgrade py-libnuma
+${conda_env_path}/bin/python3 -m pip install ogb
+${conda_env_path}/bin/python3 -m pip install --upgrade py-libnuma
 
 # [ -d $HOME/jemalloc-5.3.0 ] || { wget https://github.com/jemalloc/jemalloc/releases/download/5.3.0/jemalloc-5.3.0.tar.bz2 -O $HOME/jemalloc-5.3.0.tar.bz2 && cd $HOME && tar -xvf $HOME/jemalloc-5.3.0.tar.bz2 ; }
 # cd $HOME/jemalloc-5.3.0
