a174f847c3505c5b4acaf64d34fe5f0eb52ddb3d
zhiting zhu <zhitingz@cs.utexas.edu>
2023-04-22 00:38:26 -0500

    should be dgl
    Signed-off-by: zhiting zhu <zhitingz@cs.utexas.edu>

--- dgl/setup_deps.sh
+++ dgl/setup_deps.sh
@@ -4,7 +4,7 @@ source $HOME/miniconda3/bin/activate
 conda create -y -n dgl python=3.9
 conda install -y -n dgl -c conda-forge mkl libuv \
     'numpy>=1.16' 'pandas>=0.24' 'urllib3>=1.24.0' 'scikit-learn>=0.20' 
-conda_env_path_arr=($(conda env list | grep pt_custom_gloo))
+conda_env_path_arr=($(conda env list | grep dgl))
 conda_env_path=${conda_env_path_arr[${#conda_env_path_arr[@]}-1]}
 conda install -y -n dgl pytorch torchvision torchaudio cpuonly -c pytorch
 conda install -y -n dgl -c dglteam dgl
